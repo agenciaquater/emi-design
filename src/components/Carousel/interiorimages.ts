ed8860ec99978f117e48e76b0442585cf14a5e9c
pedro dahmer <pedrohd1213@gmail.com>
2021-08-04 16:41:01 -0300

    exclusao das imagens dos quartos sem camas

--- src/components/Carousel/interiorimages.ts
+++ src/components/Carousel/interiorimages.ts
@@ -4,16 +4,8 @@ export const interiorImages = [
         text: 'Sala de estar'
     },
     {
-        image: '/images/interior-img-3.webp',
-        text: 'Quarto 1'
-    },
-    {
         image: '/images/exterior-2.webp',
         text: 'Vista dos apartamentos final 01'
-    },
-    {
-        image: '/images/interior-img-4.webp',
-        text: 'Quarto 2'
     },
     {
         image: '/images/exterior-3.webp',
